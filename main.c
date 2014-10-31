7d1c0d18d6389ff1ddc4f805a4c5c468cb6ae341
gautegam <gaute@gamnes.no>
2014-10-31 20:10:56 +0100

    Working timer with RTC

--- main.c
+++ main.c
@@ -60,6 +60,14 @@
 
 #define MAJ_VAL_OFFSET_IN_BEACON_INFO    18                                /**< Position of the MSB of the Major Value in m_beacon_info array. */
 
+#define LFCLK_FREQUENCY           (32768UL)                               /**< LFCLK frequency in Hertz, constant. */
+#define RTC_FREQUENCY             (8UL)                                   /**< Required RTC working clock RTC_FREQUENCY Hertz. Changable. */
+#define COMPARE_COUNTERTIME       (3UL)                                   /**< Get Compare event COMPARE_TIME seconds after the counter starts from 0. */
+#define COUNTER_PRESCALER         ((LFCLK_FREQUENCY / RTC_FREQUENCY) - 1)   /* f = LFCLK/(prescaler + 1) */
+
+#define GPIO_TOGGLE_TICK_EVENT    (LED_0)                                 /**< Pin number to toggle when there is a tick event in RTC. */
+#define GPIO_TOGGLE_COMPARE_EVENT (LED_1)                                 /**< Pin number to toggle when there is compare event in RTC. */
+
 static volatile uint16_t                     m_cur_heart_rate;                          /**< Current heart rate value. */
 static volatile uint16_t major_value = (0x12345678 & 0xFFFF0000) >> 16;
 static volatile uint16_t minor_value = (0x12345678 & 0x0000FFFF);
@@ -389,6 +397,72 @@ static void timer2_init(void)
 }
 
 
+/** @brief Function starting the internal LFCLK XTAL oscillator.
+ */
+static void lfclk_config(void)
+{
+    NRF_CLOCK->LFCLKSRC            = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_LFCLKSTART    = 1;
+    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
+    {
+        //Do nothing.
+    }
+    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+}
+
+
+/** @brief Function for configuring the RTC with TICK to 100Hz and COMPARE0 to 10 sec.
+ */
+static void rtc_config(void)
+{
+    NVIC_EnableIRQ(RTC0_IRQn);                                  // Enable Interrupt for the RTC in the core.
+    NRF_RTC0->PRESCALER = COUNTER_PRESCALER;                    // Set prescaler to a TICK of RTC_FREQUENCY.
+    NRF_RTC0->CC[0]     = COMPARE_COUNTERTIME * RTC_FREQUENCY;  // Compare0 after approx COMPARE_COUNTERTIME seconds.
+
+    // Enable TICK event and TICK interrupt:
+    NRF_RTC0->EVTENSET = RTC_EVTENSET_TICK_Msk;
+    NRF_RTC0->INTENSET = RTC_INTENSET_TICK_Msk;
+
+    // Enable COMPARE0 event and COMPARE0 interrupt:
+    NRF_RTC0->EVTENSET = RTC_EVTENSET_COMPARE0_Msk;
+    NRF_RTC0->INTENSET = RTC_INTENSET_COMPARE0_Msk;
+}
+
+/** @brief: Function for handling the RTC0 interrupts.
+ * Triggered on TICK and COMPARE0 match.
+ */
+void RTC0_IRQHandler()
+{
+    if ((NRF_RTC0->EVENTS_TICK != 0) &&
+        ((NRF_RTC0->INTENSET & RTC_INTENSET_TICK_Msk) != 0))
+    {
+        NRF_RTC0->EVENTS_TICK = 0;
+        nrf_gpio_pin_toggle(GPIO_TOGGLE_TICK_EVENT);
+    }
+    
+    if ((NRF_RTC0->EVENTS_COMPARE[0] != 0) &&
+        ((NRF_RTC0->INTENSET & RTC_INTENSET_COMPARE0_Msk) != 0))
+    {
+        NRF_RTC0->EVENTS_COMPARE[0] = 0;
+        nrf_gpio_pin_toggle(GPIO_TOGGLE_COMPARE_EVENT);
+        NRF_RTC0->TASKS_CLEAR = 1;
+    }
+}
+
+
+/** @brief Function for Configuring PIN8 and PIN9 as outputs.
+ */
+static void gpio_config(void)
+{
+    nrf_gpio_cfg_output(GPIO_TOGGLE_TICK_EVENT);
+    nrf_gpio_cfg_output(GPIO_TOGGLE_COMPARE_EVENT);
+
+    nrf_gpio_pin_write(GPIO_TOGGLE_TICK_EVENT, 0);
+    nrf_gpio_pin_write(GPIO_TOGGLE_COMPARE_EVENT, 0);
+}
+
+
 /**@brief Function for initializing the BLE stack.
  *
  * @details Initializes the SoftDevice and the BLE event interrupt.
@@ -432,33 +506,36 @@ int main(void)
     
     // Use for checking when timer instead
     // ####
-    nrf_gpio_cfg_input(BUTTON_1, BUTTON_PULL);
-    timer2_init();
+    //nrf_gpio_cfg_input(BUTTON_1, BUTTON_PULL);
+    //timer2_init();
+    gpio_config();
+    lfclk_config();
+    rtc_config();
     
-    
-    leds_init();
-    ble_stack_init();
-    advertising_init();
+    //leds_init();
+    //ble_stack_init();
+    //advertising_init();
 
     // Start execution.
-    advertising_start();
+    //advertising_start();
     
     // Start handling button presses
     //err_code = app_button_enable();
     //APP_ERROR_CHECK(err_code);
     
     // Enable interrupt on Timer 2.
-    NVIC_EnableIRQ(TIMER2_IRQn);
-    __enable_irq();
+    //NVIC_EnableIRQ(TIMER2_IRQn);
+    //__enable_irq();
 
     // Start the timer.
-    NRF_TIMER2->TASKS_START = 1;
+    //NRF_TIMER2->TASKS_START = 1;
+    NRF_RTC0->TASKS_START = 1;
     // ####
 
     // Enter main loop.
     for (;;)
     {
-        power_manage();
+        //power_manage();
     }
 }
 
